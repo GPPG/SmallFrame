5b909545edc5f472b53eee4603cec1dca38f1805
郭鹏 <821403986@qq.com>
2019-12-24 14:44:27 +0800

    初始化斗地主卡牌小项目

---
+++ LandlordsCard/LandlordsCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LandlordsCard
+//
+//  Created by 郭鹏 on 2019/12/24.
+//  Copyright © 2019 郭鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
